226bae426c94b560f4c1d188cf7facd29d96413f
Eduardo Cordovil <86244886+EduardoSilvaCordovil@users.noreply.github.com>
2023-12-04 14:13:21 -0300

    Update ContaController

--- contabancaria/src/main/java/ifpa/appjpa/controller/ContaController.java
+++ contabancaria/src/main/java/ifpa/appjpa/controller/ContaController.java
@@ -1,13 +1,16 @@
 package ifpa.appjpa.controller;
 
 import java.util.List;
+import java.util.Optional;
 import ifpa.appjpa.domain.Conta;
 import ifpa.appjpa.domain.ContaRepo;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,29 +23,47 @@ public class ContaController {
     @Autowired
     private ContaRepo contaRepo;
 
-    public ContaController() {
-    }
+    public ContaController() {}
 
-    @GetMapping("/{id}")
+    @GetMapping("/buscar/{id}")
     public Conta buscar(@PathVariable Long id) {
         System.out.println("pesquisando o ID = " + id);
         return contaRepo.findById(id).orElse(new Conta());
     }
 
-    @PostMapping("/depositar/{id}")
-    public Conta depositar(@PathVariable Long id, @RequestBody Float valor) {
-        Conta conta = contaRepo.findById(id).orElse(new Conta());
-        Float novoSaldo = conta.getSaldo() + valor;
-        conta.setSaldo(novoSaldo);
-        return contaRepo.save(conta);
+    @PutMapping("/deposita/{id}")
+    public ResponseEntity<String> deposita(@PathVariable Long id, @RequestParam float valor) {
+        Optional<Conta> contaOptional = contaRepo.findById(id);
+
+        if (contaOptional.isPresent()) {
+            Conta conta = contaOptional.get();
+            float novoSaldo = conta.getSaldo() + valor;
+            conta.setSaldo(novoSaldo);
+            contaRepo.save(conta);
+            return ResponseEntity.ok("Depósito de R$" + valor + " realizado com sucesso. Novo saldo: R$" + novoSaldo);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    @PostMapping("/retirar/{id}")
-    public Conta retirar(@PathVariable Long id, @RequestBody Float valor) {
-        Conta conta = contaRepo.findById(id).orElse(new Conta());
-        Float novoSaldo = conta.getSaldo() - valor;
-        conta.setSaldo(novoSaldo);
-        return contaRepo.save(conta);
+    @PutMapping("/retirada/{id}")
+    public ResponseEntity<String> retirada(@PathVariable Long id, @RequestParam float valor) {
+        Optional<Conta> contaOptional = contaRepo.findById(id);
+
+        if (contaOptional.isPresent()) {
+            Conta conta = contaOptional.get();
+
+            if (conta.getSaldo() >= valor) {
+                float novoSaldo = conta.getSaldo() - valor;
+                conta.setSaldo(novoSaldo);
+                contaRepo.save(conta);
+                return ResponseEntity.ok("Retirada de R$" + valor + " realizada com sucesso. Novo saldo: R$" + novoSaldo);
+            } else {
+                return ResponseEntity.badRequest().body("Saldo insuficiente para a retirada.");
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/todos")
